faaeb86ee5582d5b8ed67a375ca131f78ea5468f
Abi Chang <abichangtw@gmail.com>
2024-03-02 21:50:38 +0800

    merge testing

--- src/test/java/com/abi/practice/MoneyTest.java
+++ src/test/java/com/abi/practice/MoneyTest.java
@@ -5,18 +5,27 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-public class DollarTest {
+public class MoneyTest {
     @Test
-    void multiplication() {
+    void dollar_multiplication() {
         Money five = Money.dollar(5);
         assertEquals(Money.dollar(10), five.times(2));
         assertEquals(Money.dollar(15), five.times(3));
     }
 
     @Test
+    void franc_multiplication() {
+        Money five = Money.franc(5);
+        assertEquals(Money.franc(10), five.times(2));
+        assertEquals(Money.franc(15), five.times(3));
+    }
+
+    @Test
     void equality() {
         assertEquals(Money.dollar(5), Money.dollar(5));
         assertNotEquals(Money.dollar(5), Money.dollar(6));
+        assertEquals(Money.franc(5), Money.franc(5));
+        assertNotEquals(Money.franc(5), Money.franc(6));
         assertNotEquals(Money.franc(5), Money.dollar(5));
     }
 }
