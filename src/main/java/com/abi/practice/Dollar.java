bc76130266dbf9d63fa52bf89befad96597a0cb9
Abi Chang <abichangtw@gmail.com>
2024-03-02 21:29:46 +0800

    rename variable to "money"

--- src/main/java/com/abi/practice/Dollar.java
+++ src/main/java/com/abi/practice/Dollar.java
@@ -8,8 +8,8 @@ public class Dollar extends Money {
 
     @Override
     public boolean equals(Object obj) {
-        Money dollar = (Money) obj;
-        return amount == dollar.amount;
+        Money money = (Money) obj;
+        return amount == money.amount;
     }
 
     public Dollar times(int multiplier) {
