8fd5ee3fb9448f5b32d7b3f09e0ff64fcea18983
Abi Chang <abichangtw@gmail.com>
2024-03-02 22:02:46 +0800

    inline

--- src/main/java/com/abi/practice/Franc.java
+++ src/main/java/com/abi/practice/Franc.java
@@ -8,7 +8,7 @@ public class Franc extends Money {
     }
 
     public Money times(int multiplier) {
-        return Money.franc(amount * multiplier);
+        return new Franc(amount * multiplier, "CHF");
     }
 
 }
