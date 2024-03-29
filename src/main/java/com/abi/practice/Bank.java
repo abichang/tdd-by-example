9fbaf80e441c1f14554bb0c06d6dcff090c9b9d6
Abi Chang <abichangtw@gmail.com>
2024-03-29 22:38:54 +0800

    rate from bank

--- src/main/java/com/abi/practice/Bank.java
+++ src/main/java/com/abi/practice/Bank.java
@@ -1,8 +1,13 @@
 package com.abi.practice;
 
 public class Bank {
+    int rate(String from, String to) {
+        return from.equals("CHF") && to.equals("USD")
+                ? 2 : 1;
+    }
+
     public Money reduce(Expression source, String to) {
-        return source.reduce(to);
+        return source.reduce(this, to);
     }
 
     public void addRate(String chf, String usd, int i) {
